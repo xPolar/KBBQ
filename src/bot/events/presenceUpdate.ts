d768ff35ae023bf82ed446f7586c734b1fd4268b
xPolar <polar@polar.blue>
2023-05-30 02:28:10 -0700

    stuff

--- src/bot/events/presenceUpdate.ts
+++ src/bot/events/presenceUpdate.ts
@@ -267,7 +267,7 @@ export default class PresenceUpdate extends EventHandler {
 						});
 
 					try {
-						this.client.logger.debug(18, messagePayload.messagePayload?.toString(), actionRows);
+						this.client.logger.debug(18, messagePayload.messagePayload, actionRows);
 						await this.client.api.channels.createMessage(channelId, {
 							...JSON.parse(
 								JSON.stringify({
